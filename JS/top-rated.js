5a4d46436d3ea6a4133743ca4987e92e2519474a
andrea9888 <andrea98.com@gmail.com>
2020-04-13 21:27:45 +0200

    Top rated done for tablets and laptops, added try catch to prevent error when recipes run out

--- JS/top-rated.js
+++ JS/top-rated.js
@@ -17,67 +17,85 @@ var url = new URL("https://api.edamam.com/search");
 url.search = new URLSearchParams(params).toString();
 fetch(url, requestOptions)
   .then(response => response.json())
-  .then(result => fillTopRatedContainer(result))
+  .then(result => {
+    fillTopRatedContainer(result);
+   
+  })
   .catch(error => console.log('error', error));
+
 
 function fillTopRatedContainer(result){
   let obj = result;
-  if (window.screen.width < 600){
-    showRecipeRow(obj, 1)
-    showRecipeRow(obj, 1)
-    var load = document.querySelector(".load-more");
-    load.onclick = () => showRecipeRow(obj, 1);
+  if (window.screen.width < 1200){
+    showBasedOnScreen(obj, 1);
   }
-  
+  else if (window.screen.width > 1200){
+    showBasedOnScreen(obj, 3);
+  }
 }
 
-let rowCount = 0;
+function showBasedOnScreen(obj, count){
+    rowCount = 0;
+    showRecipeRow(obj, count)
+    showRecipeRow(obj, count)
+    var load = document.querySelector(".load-more");
+    load.onclick = () => showRecipeRow(obj, count);
+}
+
+let rowCount;
+
 function showRecipeRow(result, count){
-  
-  if (topRated.hasChildNodes()){
-    for(let i=0;i<topRated.childElementCount;i++){
-      if (topRated.children[i].innerText === 'Load more'){
-        topRated.removeChild(topRated.childNodes[i]);
+  try{
+    if (topRated.hasChildNodes()){
+      for(let i=0;i<topRated.childElementCount;i++){
+        if (topRated.children[i].innerText === 'Load more'){
+          topRated.removeChild(topRated.childNodes[i]);
+        }
+      }
+
+    }
+    row = document.createElement("div");
+    row.className = "top-row";
+    for (let i=rowCount; i<rowCount+count; i++){
+      let newItem = document.createElement("div");
+      newItem.className = "top-product";
+      let image = document.createElement("img");
+      image.src = result['hits'][i]['recipe']['image'];
+      image.className = "top-images";
+
+      let text = document.createElement("div");
+      text.className = "top-text";
+      let name = document.createElement("h3");
+      name.innerText = result['hits'][i]['recipe']['label']
+      let ingr = document.createElement("p");
+      ingr.innerText = "Ingredients: " + "\n" + result['hits'][i]['recipe']['ingredientLines'].join('\n').slice(0,100) + "\n ..." ;
+      let health = document.createElement("p");
+      health.innerText = "Health Labels: " + result['hits'][i]['recipe']['healthLabels'].join(', ');
+    
+      text.appendChild(name);
+      text.appendChild(ingr);
+      text.appendChild(health);
+
+      newItem.appendChild(image);
+      newItem.appendChild(text);
+      
+      row.appendChild(newItem);
+      if (count === 1){
+        row.style.display = "flex";
       }
     }
 
+    let loadMore = document.createElement("button");
+    loadMore.innerText = "Load more";
+    loadMore.className = "load-more";
+
+    topRated.appendChild(row);
+    topRated.appendChild(loadMore);
+    load = document.querySelector(".load-more");
+    load.onclick = () => showRecipeRow(result, count);
+    rowCount+=count;
   }
-  row = document.createElement("div");
-  row.className = "top-row";
-  for (let i=rowCount; i<rowCount+count; i++){
-
-    let newItem = document.createElement("div");
-    newItem.className = "top-product";
-    let image = document.createElement("img");
-    image.src = result['hits'][i]['recipe']['image'];
-    image.className = "top-images";
-
-    let text = document.createElement("div");
-    text.className = "top-text";
-    let name = document.createElement("h3");
-    name.innerText = result['hits'][i]['recipe']['label']
-    let ingr = document.createElement("p");
-    ingr.innerText = "Ingredients: " + "\n" + result['hits'][i]['recipe']['ingredientLines'].join('\n').slice(0,100) + "\n ..." ;
-    let health = document.createElement("p");
-    health.innerText = "Health Labels: " + result['hits'][i]['recipe']['healthLabels'].join(', ');
-  
-    text.appendChild(name);
-    text.appendChild(ingr);
-    text.appendChild(health);
-
-    newItem.appendChild(image);
-    newItem.appendChild(text);
-    
-    row.appendChild(newItem);
+  catch(error){
+    console.log("No more recipes");
   }
-
-  let loadMore = document.createElement("button");
-  loadMore.innerText = "Load more";
-  loadMore.className = "load-more";
-
-  topRated.appendChild(row);
-  topRated.appendChild(loadMore);
-  load = document.querySelector(".load-more");
-  load.onclick = () => showRecipeRow(result, 1);
-  rowCount++;
 }
