2705ee4f82a1057427ac24c357be75ef2233a45e
filippilif155 <filippilif@gmail.com>
2020-04-15 01:27:49 +0200

    css part finally finished

--- JS/favourites.js
+++ JS/favourites.js
@@ -1,22 +1,41 @@
 var localStorageObj = {"items": []};
-
 var id = localStorage.getItem("favid");
-if(id && id !== "undefined"){
-    fetch(`https://api.jsonbin.io/b/${id}`, {
-            method: 'get',
-            headers: {
-                'Content-Type': 'application/json',
-                'secret-key': "$2b$10$mYqOBJTUxyvQhz.9nvQay.L0/to7DRDuWCHv5LlOwjr2yUsHK.7w."
-            }
-        }).then(function(response) {
-            return response.json();
-        }).then(function(data) {
-            localStorageObj = data;
-            console.log(localStorageObj);
-            makeTable();
-        });
+function checkLocalStorage(){
+    if(id && id !== "undefined"){
+        fetch(`https://api.jsonbin.io/b/${id}`, {
+                method: 'get',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'secret-key': "$2b$10$mYqOBJTUxyvQhz.9nvQay.L0/to7DRDuWCHv5LlOwjr2yUsHK.7w."
+                }
+            }).then(function(response) {
+                return response.json();
+            }).then(function(data) {
+                localStorageObj = data;
+                console.log(localStorageObj);
+                //makeTable();
+            });
+    }
+    else{
+        //pass
+    }
 }
 
+/*
+
+<div class="holder">
+
+    <img src="../images/test.jpg" alt="" class="image">
+
+    <div class="on-hover">
+    
+        <button class="btns show-more">MORE</button>
+        <button class="btns del-btn">DELETE</button>
+    
+    </div>
+
+</div>
+*/
 function makeTable(){
     //pass
 }
